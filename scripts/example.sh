6f906a37425704280bd5289b947969b3177a67c4
Mark St.Godard <markstgodard@gmail.com>
2016-11-11 11:12:29 -0600

    update scripts

--- scripts/example.sh
+++ scripts/example.sh
@@ -6,6 +6,11 @@ set -o pipefail
 export CNI_COMMAND=ADD
 export CNI_CONTAINERID=some-container-id
 
+NEUTRON_URL="http://192.168.56.101:9696"
+
+STATE_DIR=/tmp/cni
+mkdir -p ${STATE_DIR}
+
 mkdir -p ${PWD}/bin
 
 NET_ID="ad9845a2-64fe-4127-ab1b-7aa342a2b554"
@@ -28,7 +33,8 @@ INPUT_WRAPPER=$(cat <<END
   "name": "cni-neutron-ovs",
   "type": "gofer",
   "cniVersion": "0.2.0",
-  "neutronURL": "http://192.168.56.101:9696",
+  "neutronURL": "${NEUTRON_URL}",
+  "stateDir": "${STATE_DIR}",
   "delegate": {
     "name": "ovs",
     "type": "ovs",
